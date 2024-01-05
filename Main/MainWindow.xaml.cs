37c0ba18229ddd97e7584e1c6a560a7b447825e5
ConnectifyHub <connectifyhub@gmail.com>
2024-01-05 03:59:02 +0200

    First commit

---
+++ Main/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Main
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Login_Click(object sender, RoutedEventArgs e)
+        {
+            // Add logic to handle login
+        }
+
+        private void Register_Click(object sender, RoutedEventArgs e)
+        {
+            // Add logic to navigate to the registration page or display a registration form
+        }
+    }
+}
