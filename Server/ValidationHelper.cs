81fbda8efa58d2d43cdd5b27dc134517501a479e
ConnectifyHub <connectifyhub@gmail.com>
2024-01-10 04:41:17 +0200

    all validation functions rewrited to parts

--- Server/ValidationHelper.cs
+++ Server/ValidationHelper.cs
@@ -1,45 +1,51 @@
-﻿using System.Text.RegularExpressions;
+﻿using Server.Data.Entities;
+using System.IO;
+using System.Text.RegularExpressions;
 
 namespace Server
 {
     public static class ValidationHelper
     {
-        public static bool IsValidEmail(string email)
+        public static bool IsValidEmailAndPassword(string[] parts)
+        {
+            User user = DatabaseUtils.GetUserByEmail(parts[1]);
+            return user != null && user.Password == parts[2];
+        }
+        public static bool IsValidEmail(string[] parts)
         {
             try
             {
-                var addr = new System.Net.Mail.MailAddress(email);
-                return addr.Address == email;
-            }
-            catch
+                var addr = new System.Net.Mail.MailAddress(parts[1]);
+                return addr.Address == parts[1];
+            } catch
             {
                 return false;
             }
         }
 
-        public static bool IsStrongPassword(string password)
+        public static bool IsStrongPassword(string[] parts)
         {
             int minLength = 8;
-            bool hasUpperCase = password.Any(char.IsUpper);
-            bool hasLowerCase = password.Any(char.IsLower);
-            bool hasDigit = password.Any(char.IsDigit);
+            bool hasUpperCase = parts[1].Any(char.IsUpper);
+            bool hasLowerCase = parts[1].Any(char.IsLower);
+            bool hasDigit = parts[1].Any(char.IsDigit);
 
-            return password.Length >= minLength && hasUpperCase && hasLowerCase && hasDigit;
+            return parts[1].Length >= minLength && hasUpperCase && hasLowerCase && hasDigit;
         }
 
-        public static bool IsValidFirstName(string firstName)
+        public static bool IsValidFirstName(string[] parts)
         {
-            return Regex.IsMatch(firstName, @"^[A-Za-zА-Яа-я]+$");
+            return Regex.IsMatch(parts[1], @"^[A-Za-zА-Яа-я]+$");
         }
 
-        public static bool IsValidLastName(string lastName)
+        public static bool IsValidLastName(string[] parts)
         {
-            return Regex.IsMatch(lastName, @"^[A-Za-zА-Яа-я]+$");
+            return Regex.IsMatch(parts[1], @"^[A-Za-zА-Яа-я]+$");
         }
 
-        public static bool IsValidPhoneNumber(string phoneNumber)
+        public static bool IsValidPhoneNumber(string[] parts)
         {
-            return Regex.IsMatch(phoneNumber, @"^(\+\d{12}|\d{9})$");
+            return Regex.IsMatch(parts[1], @"^(\+\d{12}|\d{9})$");
         }
     }
 }
