0ed2345588ac0950a6f923ee12e0f181bba3297e
ConnectifyHub <connectifyhub@gmail.com>
2024-01-12 03:42:01 +0200

    Revert "Revert "added message entity""
    This reverts commit 4e4f6df8d4a877d97c423583d7a5168353fa441f.

--- Server/DatabaseUtils.cs
+++ Server/DatabaseUtils.cs
@@ -7,6 +7,7 @@ using System.Text;
 using System.Threading.Tasks;
 using Server.Data.Entities;
 using Server.Data;
+using Newtonsoft.Json;
 
 namespace Server
 {
@@ -50,6 +51,68 @@ namespace Server
             }
         }
 
+        public static User GetUserByKey(string key)
+        {
+            using (var context = GetContext())
+            {
+                var user = context.Users.FirstOrDefault(u => u.PublicIdentityKey == key);
+                if (user != null)
+                {
+                    GeneratePublicKey(user.Email);
+                }
+                return user;
+            }
+        }
+
+        public static string GetUserByKeyStringified(string key)
+        {
+            using (var context = GetContext())
+            {
+                var user = context.Users.FirstOrDefault(u => u.PublicIdentityKey == key);
+                if (user != null)
+                {
+                    return $"{user.Email}|{user.Name}|{user.Surname}";
+                }
+                return null;
+            }
+        }
+
+        static string GenerateRandomKey(int length)
+        {
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+            Random random = new Random();
+
+            string randomKey = new string(Enumerable.Repeat(chars, length)
+                .Select(s => s[random.Next(s.Length)]).ToArray());
+
+            return randomKey;
+        }
+
+        public static string GeneratePublicKey(string email)
+        {
+            string Pkey;
+            using (var context = GetContext())
+            {
+                Pkey = GenerateRandomKey(16);
+                while ((context.Users.FirstOrDefault(u => u.PublicIdentityKey == Pkey) != null)) {
+                    Pkey = GenerateRandomKey(16);
+                }
+                var user = context.Users.FirstOrDefault(u => u.Email == email);
+                user.PublicIdentityKey = Pkey;
+                context.SaveChanges();
+            }
+            return Pkey;
+        }
+
+        public static string GetPublicKey(string email, string password)
+        {
+            using (var context = GetContext())
+            {
+                var user = context.Users.FirstOrDefault(u => u.Email == email && u.Password == password);
+                return user.PublicIdentityKey;
+            }
+        }
+
         public static void UpdateUser(int userId, User updatedUser)
         {
             using (var context = GetContext())
@@ -60,7 +123,6 @@ namespace Server
                 {
                     userToUpdate.Name = updatedUser.Name;
                     userToUpdate.Surname = updatedUser.Surname;
-                    userToUpdate.Phone = updatedUser.Phone;
                     userToUpdate.Password = updatedUser.Password;
                     userToUpdate.Email = updatedUser.Email;
 
