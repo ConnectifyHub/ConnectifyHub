955120a6fc9a8250d2b8e3eaa2e40f9bda4ee7ff
ConnectifyHub <connectifyhub@gmail.com>
2024-01-12 03:36:38 +0200

    added identify key field

--- Server/Data/Entities/User.cs
+++ Server/Data/Entities/User.cs
@@ -8,13 +8,12 @@ namespace Server.Data.Entities
 {
     public class User
     {
-        public bool IsConfirmed { get; set; } = false;
         public int Id { get; set; }
         public string Name { get; set; } = null!;
-        public string Surname { get; set; }
-        public string Phone { get; set; }
+        public string Surname { get; set; } = "";
         public string Password { get; set; } = null!;
         public string Email { get; set; } = null!;
+        public string PublicIdentityKey { get; set; } = "";
 
     }
 }
